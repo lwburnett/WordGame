617a005cdf328bf74d3359abcfbb5ecbf418ac19
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-01-13 17:48:13 -0700

    Adding my word processor for the word database

---
+++ WordGame/WordDatabaseProcessor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WordDatabaseProcessor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lines = File.ReadAllLines("C:\\Users\\Luke\\Desktop\\Dev\\WordDatabase.txt");
+
+            var chosenWords = new List<string>();
+            foreach (var line in lines)
+            {
+                if (line.Trim().Length == 5)
+                    chosenWords.Add(line);
+            }
+
+            File.WriteAllLines("C:\\Users\\Luke\\Desktop\\Dev\\WordDatabase_Slimmed.txt", chosenWords);
+        }
+    }
+}
