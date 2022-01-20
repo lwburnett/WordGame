926bfdb1f3ca70e5dce460023472555add9b7b32
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-01-20 15:18:31 -0700

    More secret word database stuff

--- WordGame/WordDatabaseProcessor/Program.cs
+++ WordGame/WordDatabaseProcessor/Program.cs
@@ -12,7 +12,7 @@ namespace WordDatabaseProcessor
             var lines = File.ReadAllLines("C:\\Users\\Luke\\Desktop\\Dev\\WordDatabase.txt");
 
             var chosenLines = new List<string>();
-            foreach (var line in lines.Skip(7881))
+            foreach (var line in lines.Skip(8805))
             {
                 Console.WriteLine(line);
                 var character = Console.ReadKey();
