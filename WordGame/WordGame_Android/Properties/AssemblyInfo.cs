2a174c2eff0b106b9fd12bb1bdcfefab2d0a8dbf
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-02-07 17:41:02 -0700

    Updating some package info for android

--- WordGame/WordGame_Android/Properties/AssemblyInfo.cs
+++ WordGame/WordGame_Android/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
@@ -26,7 +25,7 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyVersion("0.8.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 #if DEBUG
